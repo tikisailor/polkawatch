4a41b2e2aedfa7a7a2afcc54cc31d32f182959dc
rafael <rvalle@privaz.io>
2022-04-04 09:25:05 +0200

    renamed controller due to typo

--- packages/lqs/src/queries/index.ts
+++ packages/lqs/src/queries/index.ts
@@ -5,7 +5,7 @@ import { GeoRegion } from './controller.distribuition.geo.region';
 import { GeoCountry } from './controller.distribuition.geo.country';
 import { NetworkProvider } from './controller.distribuition.network';
 import { ValidatorGroup } from './controller.distribuition.validator.group';
-import { AboutDataset } from './controller.distribuition.about.datasets';
+import { AboutDataset } from './controller.distribuition.about.dataset';
 import { GeoRegionEvolution } from './controller.evolution.geo.region';
 import { AboutEraEvolution } from './controller.evolution.about.era';
 
