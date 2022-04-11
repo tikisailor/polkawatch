62eec6440131c127c62a0be6cc6f16546613360a
rafael <rvalle@privaz.io>
2022-04-11 16:50:40 +0200

    unified access paths

--- packages/dapp/gatsby-node.js
+++ packages/dapp/gatsby-node.js
@@ -1,47 +1,5 @@
 const path = require('path');
 
-exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions;
-
-    createPage({
-        path: `/geography/region/:regionId/:regionName`,
-        matchPath: `/geography/region/:regionId/:regionName`,
-        component: require.resolve('./src/pages/detail/region.tsx'),
-    })
-
-    createPage({
-        path: `/geography/country/:countryId/:countryName`,
-        matchPath: `/geography/country/:countryId/:countryName`,
-        component: require.resolve('./src/pages/detail/country.tsx'),
-    })
-
-    createPage({
-        path: `/network/:networkId/:networkName`,
-        matchPath: `/network/:networkId/:networkName`,
-        component: require.resolve('./src/pages/detail/network.tsx'),
-    })
-
-    createPage({
-        path: `/operator/:operatorId/:operatorName`,
-        matchPath: `/operator/:operatorId/:operatorName`,
-        component: require.resolve('./src/pages/detail/operator.tsx'),
-    })
-
-    const { createRedirect } = actions;
-    createRedirect({
-        fromPath: "/",
-        toPath: "/dashboard/home",
-        redirectInBrowser: true,
-        isPermanent: true
-    });
-    createRedirect({
-        fromPath: "/dashboard",
-        toPath: "/dashboard/home",
-        redirectInBrowser: true,
-        isPermanent: true
-    });
-};
-
 exports.onCreateWebpackConfig = function({ actions }) {
     actions.setWebpackConfig({
         resolve: {
@@ -50,4 +8,30 @@ exports.onCreateWebpackConfig = function({ actions }) {
             }
         }
     })
+}
+
+
+exports.createPages = function({actions}){
+    const { createPage } = actions;
+
+    createPage({
+        path: `/geography/region/:regionId/:regionLabel`,
+        matchPath: `/geography/region/:regionId/:regionLabel`,
+        component: require.resolve('./src/pages/detail/region.tsx'),
+    });
+    createPage({
+        path: `/geography/country/:countryId/:countryLabel`,
+        matchPath: `/geography/country/:countryId/:countryLabel`,
+        component: require.resolve('./src/pages/detail/country.tsx'),
+    });
+    createPage({
+        path: `/network/provider/:networkId/:networkLabel`,
+        matchPath: `/network/provider/:networkId/:networkLabel`,
+        component: require.resolve('./src/pages/detail/network.tsx'),
+    });
+    createPage({
+        path: `/validation/operator/:operatorId/:operatorLabel`,
+        matchPath: `/validation/operator/:operatorId/:operatorLabel`,
+        component: require.resolve('./src/pages/detail/operator.tsx'),
+    });
 }
