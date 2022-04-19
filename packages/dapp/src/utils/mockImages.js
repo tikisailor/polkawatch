c7720ea06e033fbb23772de0a9f0cb061aa305d7
tikisailor <40813246+tikisailor@users.noreply.github.com>
2022-04-19 20:19:51 +0800

    clean up unused files

--- packages/dapp/src/utils/mockImages.js
+++
@@ -1,5 +0,0 @@
-// ----------------------------------------------------------------------
-
-export const mockImgCover = (index) => `/mock-images/covers/cover_${index}.jpg`;
-export const mockImgProduct = (index) => `/mock-images/products/product_${index}.jpg`;
-export const mockImgAvatar = (index) => `/mock-images/avatars/avatar_${index}.jpg`;
